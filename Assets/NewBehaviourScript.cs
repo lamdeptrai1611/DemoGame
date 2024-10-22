44bcf283755a739f62de4f99146c4e502d2c1562
LamVi1611 <lamvikhanh1611@gmail.com>
2024-10-22 23:30:17 +0700

    codemoi

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Awake()
+    {
+        Debug.Log("Game Object Awake!");
+        Debug.LogWarning("This is a warning massage!");
+        Debug.LogError("This is a error massage!");
+    }
+
+}
+
